ba9868d7150119c392f1bb7aaf729d6ee9e2d350
Júlio Fortunato <juliofortunato58@gmail.com>
2024-09-11 20:03:11 -0300

    ✨ feat: add ResourceList component

---
+++ app/_components/resource-list.tsx
@@ -0,0 +1,24 @@
+import { ComicList } from "../_types/comics";
+import { EventList } from "../_types/events";
+import { SeriesList } from "../_types/series";
+import { StoryList } from "../_types/stories";
+
+interface ResourceListProps {
+  list: ComicList | EventList | SeriesList | StoryList | undefined;
+}
+
+const ResourceList = ({ list }: ResourceListProps) => {
+  if (!list) return null;
+
+  return (
+    <ul className="space-y-1">
+      {list.items.map((item, idx) => (
+        <li className="text-white" key={idx}>
+          {item.name}
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default ResourceList;
