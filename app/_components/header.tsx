984f77444f931d813a5c9f116495e8c23bb74d4f
Júlio Fortunato <juliofortunato58@gmail.com>
2024-09-11 21:29:33 -0300

    💎 style: make header sticky at top

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -4,13 +4,13 @@ import Link from "next/link";
 
 const Header = () => {
   return (
-    <div className="flex items-center justify-between px-7 py-5 text-gray-900">
+    <header className="sticky top-0 z-10 flex items-center justify-between bg-white px-7 py-5 text-gray-900">
       <MenuIcon />
       <Link href="/">
         <Image src="/logo.svg" alt="Marvel" width={71} height={26} />
       </Link>
       <SearchIcon />
-    </div>
+    </header>
   );
 };
 
--- app/page.tsx
+++ app/page.tsx
@@ -5,14 +5,14 @@ export default function Home() {
   return (
     <>
       <Header />
-      <div className="px-7">
+      <div className="space-y-4 px-7">
         <div>
           <h2 className="text-sm font-semibold text-gray-400">
             Bem vindo ao Marvel Heroes
           </h2>
           <h1 className="text-4xl font-black">Escolha o seu personagem</h1>
-          <CharacterList />
         </div>
+        <CharacterList />
       </div>
     </>
   );
