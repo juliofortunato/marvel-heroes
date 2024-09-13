daeb35b615dfe6e425537387f4718c4409a085b9
Júlio Fortunato <juliofortunato58@gmail.com>
2024-09-13 16:14:49 -0300

    🔨 refactor: handling error on Stats component

--- app/_components/stats.tsx
+++ app/_components/stats.tsx
@@ -11,13 +11,18 @@ import {
 import useFilters from "../_hooks/useFilters";
 import usePagination from "../_hooks/usePagination";
 import { useCharacters } from "../_services/character";
+import Error from "./error";
 import Loading from "./loading";
 import { ChartContainer } from "./ui/chart";
 
 const Stats = () => {
   const { currentPage } = usePagination();
   const { search, orderBy } = useFilters();
-  const { data, isLoading } = useCharacters(currentPage - 1, search, orderBy);
+  const { data, isLoading, error } = useCharacters(
+    currentPage - 1,
+    search,
+    orderBy,
+  );
   const formattedData = useMemo(
     () =>
       data?.results.map((character) => ({
@@ -30,6 +35,8 @@ const Stats = () => {
   formattedData?.sort((a, b) => b.value - a.value);
 
   if (isLoading) return <Loading />;
+
+  if (error) return <Error message={error.message} />;
 
   return (
     <ChartContainer config={{}} className="bg-red h-full w-full py-8 pb-10">
